7f808e7aa7508f6a1d6dd7867e9aa963e3a68fa2
gerardmolano <g.molano11@gmail.com>
2022-03-07 20:33:48 +0800

    youtube solution

--- python/longest_palindrome_substring.py
+++ python/longest_palindrome_substring.py
@@ -1,30 +1,26 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        palindrome = ""
+        res = ''
+        res_len = 0
 
         for i in range(len(s)):
-            j = len(s) - 1
+            # odd
+            res, res_len = self.checkPalindrome(s, i, i, res, res_len)
+            # even
+            res, res_len = self.checkPalindrome(s, i, i + 1, res, res_len)
 
-            while j >= 0:
-                if s[j] == s[i]:
-                    cur = s[i:j + 1]
-
-                    if self.is_palindrome(cur):
-                        if (len(palindrome) < len(cur)):
-                            palindrome = cur
-                            break
-
-                j = j - 1;
-
-            if  (j - i) > (len(s) - j):
-                break
-
-        return palindrome
+        return res
     
-    def is_palindrome(self, s: str) -> bool:
-        l = list(s)
-        l.reverse()
-        return (''.join(l) == s)
+    def checkPalindrome(self, s, l, r, res, res_len):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            if (r - l + 1) > res_len:
+                res = s[l:r + 1]
+                res_len = r - l + 1
+            
+            l -= 1
+            r += 1
+        
+        return (res, res_len)
 
 # test here
-print(Solution().longestPalindrome("cmmrracelnclsbtdmuxtfiyahrvxuwreyorosyqapfpnsntommsujibzwhgugwtvxsdsltiiyymiofbslwbwevmjrsbbssicnxptvwmsmiifypoujftxylpyvirfueagprfyyydxeiftathaygmolkcwoaavmdmjsuwoibtuqoewaexihispsshwnsurjopdwttlzyqdbkypvjsbuidsdnpgklhwfnqdvlffcysnxeywvwvblatmxbflnuykhfhjptenhcxqinomlwalvlezefqybpuepbnymzlruuirpiatqgjgcnfmrlzshauoxuoqopcikogfwpssjdeplytcapmujyvgtfmmolnuadpwblgmcaututcrwsqrlpaaqobjfnhudmsulztbdkxpfejavastxejtpbqfftdtcdhvtpbzfuqcwkxtldtjycreimiujtxudtmokcoebhodbkgkgxjzrgyuqhozqtidltodlkziyofdeszwiobkwesdijxbbagguxvofvtphqxgvidqfkljufgubjmjllxoanbizwtedykwmneaosopynzlzvrlqcmyaahdcagfatlhwtgqxsyxwjhexfiplwtrtydjzrsysrcwszlntwrpgfedhgjzhztffqnjotlfudvczwfkhuwmdzcqgrmfttwaxocohtuscdxwfvhcymjpkqexusdaccw"))
+print(Solution().longestPalindrome("b"))
